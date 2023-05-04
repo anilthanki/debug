7d3d350466228684b6bba578e9cf7e3e1a205292
Anil Thanki <thanki.anil@gmail.com>
2023-05-04 21:59:31 +0100

    Update datalib_from_localfiles.py

--- python/datalib_from_localfiles.py
+++ python/datalib_from_localfiles.py
@@ -85,7 +85,7 @@ def upload_files_to_lib(gi, lib_id, source_dir, galaxy_path, root_folder):
 			else:
 				new_dirname = directory_path.replace(source_dir+"/", "")
 				log.debug('new_dirname %s', new_dirname)
-				folder = gi.libraries.get_folders(lib_id, name=directory_name)
+				folder = gi.libraries.get_folders(lib_id, name=new_dirname)
 				log.debug('folder %s', folder)
 				
 				folder = gi.libraries.create_folder(lib_id, folder_name=directory_name, base_folder_id=folder[0]['id'])
