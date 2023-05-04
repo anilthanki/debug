e47210b1c853b78bbde4a3936ccda99ce6a7f147
Anil Thanki <thanki.anil@gmail.com>
2023-05-04 16:26:26 +0100

    Update datalib_from_localfiles.py

--- python/datalib_from_localfiles.py
+++ python/datalib_from_localfiles.py
@@ -65,50 +65,51 @@ def create_new_libs(gi, lib_name, lib_desc, lib_synopsis):
 def upload_files_to_lib(gi, lib_id, source_dir, galaxy_path, root_folder):
 	
 	log.debug('upload_files_to_lib')
-	# root_folder = gi.libraries.get_libraries(lib_id)[0]['root_folder_id']
+	 
+	root_folder = gi.libraries.get_libraries(lib_id)[0]['root_folder_id']
 	for directory_path, directory_names, file_names in os.walk(source_dir):
 		log.debug('main loop ..')
 			
 		# Recreate the directory structure
-# 		for directory_name in directory_names:
-# 			log.debug('directory_name %s', directory_name)
-# 			log.debug('root_folder %s', root_folder)
-# 			log.debug('directory_path %s', directory_path)
-# 			directory_path = os.path.join(galaxy_path, directory_name)
-# 			try:
-# 			    # Create the folder in the library
-# 			    gi.libraries.get_folders(lib_id, name=directory_name)[0]['id']
-# 			    folder = gi.libraries.create_folder(lib_id, folder_name=directory_name, base_folder_id=root_folder)
-# 			    # Set the parent folder ID for the next iteration
+		for directory_name in directory_names:
+			log.debug('directory_name %s', directory_name)
+			log.debug('root_folder %s', root_folder)
+			log.debug('directory_path %s', directory_path)
+			directory_path = os.path.join(galaxy_path, directory_name)
+			try:
+			    # Create the folder in the library
+			    gi.libraries.get_folders(lib_id, name=directory_name)[0]['id']
+			    folder = gi.libraries.create_folder(lib_id, folder_name=directory_name, base_folder_id=root_folder)
+			    # Set the parent folder ID for the next iteration
 # 			    root_folder = folder[0]['id']
-# 			except Exception:
-# 			    # Folder already exists, get its ID and set the parent folder ID for the next iteration
-# 			    folder = gi.libraries.get_folders(lib_id, name=directory_name)
+			except Exception:
+			    # Folder already exists, get its ID and set the parent folder ID for the next iteration
+			    folder = gi.libraries.get_folders(lib_id, name=directory_name)
 # 			    root_folder = folder[0]['id']
 	
-		for file_name in file_names:
-			if not file_name.startswith('.'):
-				log.debug('file_name %s', file_name)
+# 		for file_name in file_names:
+# 			if not file_name.startswith('.'):
+# 				log.debug('file_name %s', file_name)
 			
-	# 			log.debug('directory_path %s', directory_path)
-	# 			file_path = os.path.join(galaxy_path,file_name)
-	# 			log.debug('file_path %s', file_path)
-	# 			dirname = os.path.basename(directory_path)
-	# 			log.debug('dirname %s', dirname)
-				log.debug('directory_path %s', directory_path)
-				log.debug('source_dir %s', source_dir)
-				log.debug('galaxy_path %s', galaxy_path)
-				if directory_path == source_dir:
-					new_dirname = ""
-				else:
-					new_dirname = directory_path.replace(source_dir+"/", "")
-				log.debug('new_dirname %s', new_dirname)
-				new_path = os.path.join(galaxy_path, new_dirname)
-				log.debug('new_path %s', new_path)
-				new_file_path = os.path.join(new_path, file_name)
-				log.debug('new file_path %s', new_file_path)
-				# Upload the file to the library
-				gi.libraries.upload_from_galaxy_filesystem(lib_id, new_file_path, preserve_dirs=True)
+# 	# 			log.debug('directory_path %s', directory_path)
+# 	# 			file_path = os.path.join(galaxy_path,file_name)
+# 	# 			log.debug('file_path %s', file_path)
+# 	# 			dirname = os.path.basename(directory_path)
+# 	# 			log.debug('dirname %s', dirname)
+# 				log.debug('directory_path %s', directory_path)
+# 				log.debug('source_dir %s', source_dir)
+# 				log.debug('galaxy_path %s', galaxy_path)
+# 				if directory_path == source_dir:
+# 					new_dirname = ""
+# 				else:
+# 					new_dirname = directory_path.replace(source_dir+"/", "")
+# 				log.debug('new_dirname %s', new_dirname)
+# 				new_path = os.path.join(galaxy_path, new_dirname)
+# 				log.debug('new_path %s', new_path)
+# 				new_file_path = os.path.join(new_path, file_name)
+# 				log.debug('new file_path %s', new_file_path)
+# 				# Upload the file to the library
+# 				gi.libraries.upload_from_galaxy_filesystem(lib_id, new_file_path, preserve_dirs=True)
 
 
 def main():
